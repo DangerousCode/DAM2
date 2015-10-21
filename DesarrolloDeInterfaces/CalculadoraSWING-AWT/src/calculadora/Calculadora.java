923312284ba0167eaa83c14f73e5e58208447175
ax25 <asantosq1@gmail.com>
2015-10-21 21:17:18 +0200

    try of calc

--- DesarrolloDeInterfaces/CalculadoraSWING-AWT/src/calculadora/Calculadora.java
+++ DesarrolloDeInterfaces/CalculadoraSWING-AWT/src/calculadora/Calculadora.java
@@ -1,7 +1,38 @@
 package calculadora;
 
-public class Calculadora {
-	public void main(String[] args){
-		new Gui();
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.SpringLayout;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+
+public class Calculadora extends JFrame {
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Calculadora frame = new Calculadora();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Calculadora() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
 	}
 }
