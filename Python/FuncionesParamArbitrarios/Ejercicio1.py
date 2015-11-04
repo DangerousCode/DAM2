f0d2e95728fdd8045213ca4c813abc38d8a5f13e
ax25 <asantosq1@gmail.com>
2015-11-04 17:52:55 +0100

    p

---
+++ Python/FuncionesParamArbitrarios/Ejercicio1.py
@@ -0,0 +1,6 @@
+__author__ = 'AlumnoT'
+#-*-coding:utf-8-*-
+'''Vamos a crear una función a la que le vamos a pasar un número fijo y a continuación, un número indeterminado de
+parámetros formados mínimo por 5 cifras, todas ellas distintas.
+Dicha función deberá devolvernos la concatenación del número que hemos puesto como fijo
+más los que hemos ido poniendo como indeterminados.'''
