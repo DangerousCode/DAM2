002d27f512d794abf4ced4be0f25de4d511db040
ax25 <asantosq1@gmail.com>
2015-11-06 13:21:49 +0100

    Finalized OPERACIONES XML

--- AccesoADatos/OperacionesXML/src/ejercicio4/Ejercicio4.java
+++ AccesoADatos/OperacionesXML/src/ejercicio4/Ejercicio4.java
@@ -27,6 +27,53 @@ public class Ejercicio4 {
 			
 			/*2. añadir una nueva etiqueta Nombre1 en todos los tiposplatos*/
 			System.out.println("\nSegunda parte \n");
+			Iterator it2=elementos.iterator();
+			while(it2.hasNext()){
+				Element nombre1=new Element("nombre1");
+				Element comida=(Element) it2.next();
+				Element tipoplatos=comida.getChild("tipoplatos");
+				tipoplatos.addContent(nombre1);
+			}
+			new XMLOutputter().output(doc,System.out);
+			
+			/*3. modificar el texto de las nuevas etiquetas nombre1
+ horchata ,vino, cava respectivamente*/
+			System.out.println("\nTercera parte\n");
+			for(int i=0;i<3;i++){
+				Element nombre1=elementos.get(i).getChild("tipoplatos").getChild("nombre1");
+				switch(i){
+				case 0:
+					nombre1.setText("horchata");
+					break;
+				case 1:
+					nombre1.setText("vino");
+					break;
+				case 2:
+					nombre1.setText("cava");
+					break;
+				}
+			}
+			new XMLOutputter().output(doc,System.out);
+			
+			/*4. Eliminar del menú las comidas cuyo plato sea postre y 
+			 * su nombre coincida con crema con nueces*/
+			System.out.print("\nCuarta parte\n");
+			Iterator it3=elementos.iterator();
+			while(it3.hasNext()){
+				Element comida=(Element)it3.next();
+				if(comida.getAttributeValue("plato").equals("postre")){
+					Element tipoplatos=comida.getChild("tipoplatos");
+					List<Element> nombres=tipoplatos.getChildren("nombre");
+					Iterator it4=nombres.iterator();
+					while(it4.hasNext()){
+						Element nombre=(Element)it4.next();
+						if(nombre.getText().equals("crema con nueces")){
+							tipoplatos.removeContent(nombre);
+						}
+					}
+				}
+			}
+			new XMLOutputter().output(doc,System.out);
 		}catch(Exception e){
 			e.printStackTrace();
 		}
