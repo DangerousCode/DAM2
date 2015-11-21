0d45a8945e3fb679748f6d993f0118af2b34daa0
ax25 <asantosq1@gmail.com>
2015-11-21 14:07:35 +0100

    finalized ejercicio2 xpath aula

--- AccesoADatos/Ejercicio 2 Xpath Aula/src/ejercicio/pkg2/xpath/aula/Ejercicio3.java
+++ AccesoADatos/Ejercicio 2 Xpath Aula/src/ejercicio/pkg2/xpath/aula/Ejercicio3.java
@@ -12,6 +12,7 @@ package ejercicio.pkg2.xpath.aula;
 public class Ejercicio3 {
     public static void main(String[] args){
         String ej="ejercicio3.xml";
+        System.out.println("\nEJERCICIO 3 SIN ABREVIAR\n");
         herramientas.Herramientas.consultasXPATH(ej, "/child::ies/child::modulos/child::modulo/child::nombre");
         System.out.println("\nEjercicio2\n");
         herramientas.Herramientas.consultasXPATH(ej, "/child::ies/child::modulos/child::modulo/child::ciclo[self::ciclo='ASIR']/parent::modulo/child::nombre");
@@ -21,5 +22,15 @@ public class Ejercicio3 {
         herramientas.Herramientas.consultasXPATH(ej, "//child::modulo[child::ciclo='ASIR' and child::curso='1']/child::nombre");
         System.out.println("\nEjercicio5\n");
         herramientas.Herramientas.consultasXPATH(ej, "//ancestor::modulo[child::horasSemanales>'3']/child::horasSemanales");
+        System.out.println("\n\n---------------EJERCICIO 3 ABREVIADO---------------\n");
+        herramientas.Herramientas.consultasXPATH(ej, "/ies/modulos/modulo/nombre");
+        System.out.println("\nEjercicio2\n");
+        herramientas.Herramientas.consultasXPATH(ej, "//modulo[ciclo='ASIR']/nombre");
+        System.out.println("\nEjercicio3\n");
+        herramientas.Herramientas.consultasXPATH(ej, "//modulo[horasSemanales<'5']/nombre");
+        System.out.println("\nEjercicio4\n");
+        herramientas.Herramientas.consultasXPATH(ej, "//modulo[curso='1' and ciclo='ASIR']/nombre");
+        System.out.println("\nEjercicio5\n");
+        herramientas.Herramientas.consultasXPATH(ej, "//modulo[horasSemanales>'3']/horasSemanales");
     }
 }
